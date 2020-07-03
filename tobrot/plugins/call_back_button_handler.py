1339a909023a4d4a4b592eade877f8bd6c2c91c4
surlogu <45895919+surlogu@users.noreply.github.com>
2020-07-03 21:40:40 +0530

    Update call_back_button_handler.py

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -39,67 +39,114 @@ async def button(bot, update: CallbackQuery):
     cb_data = update.data
     
     if cb_data.startswith("leech"):
-        # get link from the incoming message
-        dl_url, cf_name, _, _ = await extract_link(update.message.reply_to_message, "LEECH")
-        LOGGER.info(dl_url)
-        LOGGER.info(cf_name)
-        if dl_url is not None:
-            await update.message.edit_text("extracting links")
-            # start the aria2c daemon
-            aria_i_p = await aria_start()
-            LOGGER.info(aria_i_p)
-            if "_" in cb_data:
-                current_user_id = update.from_user.id
-                # create an unique directory
-                new_download_location = os.path.join(
-                    DOWNLOAD_LOCATION,
-                    str(current_user_id),
-                    str(time.time())
-                )
-                # create download directory, if not exist
-                if not os.path.isdir(new_download_location):
-                    os.makedirs(new_download_location)
-                await update.message.edit_text("trying to download")
-                # try to download the "link"
-                sagtus, err_message = await call_apropriate_function(
-                    aria_i_p,
-                    dl_url,
-                    new_download_location,
-                    update.message,
-                    int(cb_data.split("_")[2])
-                    # maybe IndexError / ValueError might occur,
-                    # we don't know, yet!!
-                )
-                if not sagtus:
-                    # if FAILED, display the error message
-                    await update.message.edit_text(err_message)
-            else:
-                is_zip = False
-                if "a" in cb_data:
-                    is_zip = True
-                current_user_id = update.from_user.id
-                # create an unique directory
-                new_download_location = os.path.join(
-                    DOWNLOAD_LOCATION,
-                    str(current_user_id),
-                    str(time.time())
-                )
-                # create download directory, if not exist
-                if not os.path.isdir(new_download_location):
-                    os.makedirs(new_download_location)
-                await update.message.edit_text("trying to download")
-                # try to download the "link"
-                sagtus, err_message = await call_apropriate_function(
-                    aria_i_p,
-                    dl_url,
-                    new_download_location,
-                    update.message,
-                    is_zip
-                )
-                if not sagtus:
-                    # if FAILED, display the error message
-                    await update.message.edit_text(err_message)
-    
+        i_m_sefg = await message.reply_text("processing", quote=True)
+    is_zip = False
+    is_unzip = False
+    is_unrar = False
+    is_untar = False
+    if len(message.command) > 1:
+        if message.command[1] == "archive":
+            is_zip = True
+        elif message.command[1] == "unzip":
+            is_unzip = True
+        elif message.command[1] == "unrar":
+            is_unrar = True
+        elif message.command[1] == "untar":
+            is_untar = True
+    # get link from the incoming message
+    dl_url, cf_name, _, _ = await extract_link(message.reply_to_message, "LEECH")
+    LOGGER.info(dl_url)
+    LOGGER.info(cf_name)
+    if dl_url is not None:
+        await i_m_sefg.edit_text("extracting links")
+        # start the aria2c daemon
+        aria_i_p = await aria_start()
+        LOGGER.info(aria_i_p)
+        current_user_id = message.from_user.id
+        # create an unique directory
+        new_download_location = os.path.join(
+            DOWNLOAD_LOCATION,
+            str(current_user_id),
+            str(time.time())
+        )
+        # create download directory, if not exist
+        if not os.path.isdir(new_download_location):
+            os.makedirs(new_download_location)
+        await i_m_sefg.edit_text("trying to download")
+        # try to download the "link"
+        sagtus, err_message = await call_apropriate_function(
+            aria_i_p,
+            dl_url,
+            new_download_location,
+            i_m_sefg,
+            is_zip,
+            cf_name,
+            is_unzip,
+            is_unrar,
+            is_untar
+        )
+        
+        if not sagtus:
+            # if FAILED, display the error message
+            await i_m_sefg.edit_text(err_message)
+    else:
+        await i_m_sefg.edit_text(
+            "What you have entered is wrong❌ \nPlease read /help \n"
+            f"<b>API Error❗</b>: {cf_name}"
+        )    
+    elif cb_data.startswith("gleech"):
+        i_m_sefg = await message.reply_text("processing", quote=True)
+    is_zip = False
+    is_unzip = False
+    is_unrar = False
+    is_untar = False
+    if len(message.command) > 1:
+        if message.command[1] == "archive":
+            is_zip = True
+        elif message.command[1] == "unzip":
+            is_unzip = True
+        elif message.command[1] == "unrar":
+            is_unrar = True
+        elif message.command[1] == "untar":
+            is_untar = True
+    # get link from the incoming message
+    dl_url, cf_name, _, _ = await extract_link(message.reply_to_message, "GLEECH")
+    LOGGER.info(dl_url)
+    LOGGER.info(cf_name)
+    if dl_url is not None:
+        await i_m_sefg.edit_text("extracting links")
+        # start the aria2c daemon
+        aria_i_p = await aria_start()
+        LOGGER.info(aria_i_p)
+        current_user_id = message.from_user.id
+        # create an unique directory
+        new_download_location = os.path.join(
+            DOWNLOAD_LOCATION,
+            str(current_user_id),
+            str(time.time())
+        )
+        # create download directory, if not exist
+        if not os.path.isdir(new_download_location):
+            os.makedirs(new_download_location)
+        await i_m_sefg.edit_text("trying to download")
+        # try to download the "link"
+        await call_apropriate_function_g(
+            aria_i_p,
+            dl_url,
+            new_download_location,
+            i_m_sefg,
+            is_zip,
+            cf_name,
+            is_unzip,
+            is_unrar,
+            is_untar,
+            message
+        )
+    else:
+        await i_m_sefg.edit_text(
+            "What you have entered is wrong❌ \nPlease read /help \n"
+            f"<b>API Error❗</b>: {cf_name}"
+        )
     elif cb_data.startswith("ytdl"):
         i_m_sefg = await update.message.edit_text("processing")
         # LOGGER.info(message)
