7f1653688d2e86197c4b6287df36805b269e40db
surlogu <45895919+surlogu@users.noreply.github.com>
2020-07-03 20:18:29 +0530

    Create custom_filters.py

---
+++ tobrot/helper_funcs/custom_filters.py
@@ -0,0 +1,31 @@
+
+from pyrogram import (
+    Filters,
+    Message
+)
+
+
+def message_filter_f(f, m: Message):
+    return bool(
+        # below checks if it is a valid link
+        (
+            (
+                ("http" in m.text) or
+                ("magnet:" in m.text)
+            ) or (
+            # below checks the TORRENT detection part
+            m.document and
+            m.document.file_name.upper().endswith(".TORRENT")
+        ) and (
+                # to avoid conflicts with
+                # popular @LinkToFilesBot (s)
+                ".html" not in m.text
+            )
+        )
+    )
+
+
+message_fliter = Filters.create(
+    func=message_filter_f,
+    name="TstMesgFilter"
+)
