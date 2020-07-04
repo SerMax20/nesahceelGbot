83a9a1f8ef165d73523962efb99cb28f1171cea5
surlogu <45895919+surlogu@users.noreply.github.com>
2020-07-04 15:08:44 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -29,7 +29,6 @@ from tobrot.helper_funcs.download_from_link import request_download
 from tobrot.helper_funcs.display_progress import progress_for_pyrogram
 from tobrot.helper_funcs.youtube_dl_extractor import extract_youtube_dl_formats
 from tobrot.helper_funcs.admin_check import AdminCheck
-from tobrot.helper_funcs.create_r_o_m import get_markup
 
 async def incoming_purge_message_f(client, message):
     """/purge command"""
@@ -44,24 +43,116 @@ async def incoming_purge_message_f(client, message):
     
 async def incoming_message_f(client, message):
     """/leech command"""
-    i_m_sefg = await message.reply_text("checking ", quote=True)
-    t_, rm_ = await get_markup(message)
-    await i_m_sefg.edit_text(
-        text=t_,
-        reply_markup=rm_,
-        disable_web_page_preview=True
-    )
+    i_m_sefg = await message.reply_text("processing", quote=True)
+    is_zip = False
+    is_unzip = False
+    is_unrar = False
+    is_untar = False
+    if len(message.command) > 1:
+        if message.command[1] == "archive":
+            is_zip = True
+        elif message.command[1] == "unzip":
+            is_unzip = True
+        elif message.command[1] == "unrar":
+            is_unrar = True
+        elif message.command[1] == "untar":
+            is_untar = True
+    # get link from the incoming message
+    dl_url, cf_name, _, _ = await extract_link(message.reply_to_message, "LEECH")
+    LOGGER.info(dl_url)
+    LOGGER.info(cf_name)
+    if dl_url is not None:
+        await i_m_sefg.edit_text("extracting links")
+        # start the aria2c daemon
+        aria_i_p = await aria_start()
+        LOGGER.info(aria_i_p)
+        current_user_id = message.from_user.id
+        # create an unique directory
+        new_download_location = os.path.join(
+            DOWNLOAD_LOCATION,
+            str(current_user_id),
+            str(time.time())
+        )
+        # create download directory, if not exist
+        if not os.path.isdir(new_download_location):
+            os.makedirs(new_download_location)
+        await i_m_sefg.edit_text("trying to download")
+        # try to download the "link"
+        sagtus, err_message = await call_apropriate_function(
+            aria_i_p,
+            dl_url,
+            new_download_location,
+            i_m_sefg,
+            is_zip,
+            cf_name,
+            is_unzip,
+            is_unrar,
+            is_untar
+        )
+        
+        if not sagtus:
+            # if FAILED, display the error message
+            await i_m_sefg.edit_text(err_message)
+    else:
+        await i_m_sefg.edit_text(
+            "What you have entered is wrong❌ \nPlease read /help \n"
+            f"<b>API Error❗</b>: {cf_name}"
+        )
 #
 async def incoming_gdrive_message_f(client, message):
     """/gleech command"""
-    i_m_sefg = await message.reply_text("checking ", quote=True)
-    t_, rm_ = await get_markup(message)
-    await i_m_sefg.edit_text(
-        text=t_,
-        reply_markup=rm_,
-        disable_web_page_preview=True
-    )
-       
+    i_m_sefg = await message.reply_text("processing", quote=True)
+    is_zip = False
+    is_unzip = False
+    is_unrar = False
+    is_untar = False
+    if len(message.command) > 1:
+        if message.command[1] == "archive":
+            is_zip = True
+        elif message.command[1] == "unzip":
+            is_unzip = True
+        elif message.command[1] == "unrar":
+            is_unrar = True
+        elif message.command[1] == "untar":
+            is_untar = True
+    # get link from the incoming message
+    dl_url, cf_name, _, _ = await extract_link(message.reply_to_message, "GLEECH")
+    LOGGER.info(dl_url)
+    LOGGER.info(cf_name)
+    if dl_url is not None:
+        await i_m_sefg.edit_text("extracting links")
+        # start the aria2c daemon
+        aria_i_p = await aria_start()
+        LOGGER.info(aria_i_p)
+        current_user_id = message.from_user.id
+        # create an unique directory
+        new_download_location = os.path.join(
+            DOWNLOAD_LOCATION,
+            str(current_user_id),
+            str(time.time())
+        )
+        # create download directory, if not exist
+        if not os.path.isdir(new_download_location):
+            os.makedirs(new_download_location)
+        await i_m_sefg.edit_text("trying to download")
+        # try to download the "link"
+        await call_apropriate_function_g(
+            aria_i_p,
+            dl_url,
+            new_download_location,
+            i_m_sefg,
+            is_zip,
+            cf_name,
+            is_unzip,
+            is_unrar,
+            is_untar,
+            message
+        )
+    else:
+        await i_m_sefg.edit_text(
+            "What you have entered is wrong❌ \nPlease read /help \n"
+            f"<b>API Error❗</b>: {cf_name}"
+        )
 
 async def incoming_youtube_dl_f(client, message):
     """ /ytdl command """
