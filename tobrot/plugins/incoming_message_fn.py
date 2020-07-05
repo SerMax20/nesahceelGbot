0ae86a91583d6cd9e59978bf701220642aa0eab8
surlogu <45895919+surlogu@users.noreply.github.com>
2020-07-05 18:29:01 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -1,10 +1,6 @@
-# Copyright (C) 2020 by surlogu@Github, < https://github.com/surlogu>.
-#
-# This file is part of < https://github.com/surlogu/AsEnLEECH > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/surlogu/AsEnLEECH/blob/master/LICENSE >
-#
-# All rights reserved.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K & GautamKumar
 
 import logging
 logging.basicConfig(
